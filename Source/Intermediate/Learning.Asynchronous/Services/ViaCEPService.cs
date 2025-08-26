91a53952a87e308de6abb0223e6a79d2437b72ec
Brendo Pereira Viegas <159083802+Viegasync@users.noreply.github.com>
2025-08-26 01:49:06 -0300

    feat: add ViaCEPService

---
+++ Source/Intermediate/Learning.Asynchronous/Services/ViaCEPService.cs
@@ -0,0 +1,47 @@
+﻿namespace Learning.Asynchronous.Services;
+
+/// <summary>
+/// Serviço que fornece acesso à API.
+/// </summary>
+internal sealed class ViaCEPService(HttpClient http) : IDisposable
+{
+    /// <summary>
+    /// Realiza uma chamada assíncrona à API e, 
+    /// conforme o CEP informado, obtém os dados.
+    /// </summary>
+    /// <exception cref="HttpRequestException"></exception>
+    internal async Task<AddressResponse?> GetAddressAsync(string zipCode, CancellationToken token = default)
+    {
+        ValidateZipCode(zipCode);
+        HttpResponseMessage response = await http
+            .GetAsync($"https://viacep.com.br/ws/{zipCode}/json/", token);
+
+        AddressResponse? address = await response.Content
+            .ReadFromJsonAsync<AddressResponse>(token);
+
+        return !response.IsSuccessStatusCode || address?.Error == "true"
+            ? throw new HttpRequestException("Address not found.")
+            : address;
+    }
+
+    /// <summary>
+    /// Valida e formata um CEP, 
+    /// removendo caracteres não numéricos.
+    /// </summary>
+    /// <exception cref="FormatException"></exception>
+    private static string ValidateZipCode(string zipCode)
+    {
+        string zipCodeFormatted = new([.. zipCode
+            .Where(char.IsDigit)]);
+
+        return zipCodeFormatted.Length != 8
+            ? throw new FormatException("ZipCode is invalid.")
+            : zipCodeFormatted;
+    }
+
+    /// <summary>
+    /// Libera os recursos utilizados
+    /// pelo <see cref="HttpClient"/> interno.
+    /// </summary>
+    public void Dispose() => http.Dispose();
+}
