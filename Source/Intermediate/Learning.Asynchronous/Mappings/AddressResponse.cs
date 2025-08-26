942d2088c033a127331b6954a6c0ee8cf67d1e82
Brendo Pereira Viegas <159083802+Viegasync@users.noreply.github.com>
2025-08-26 01:48:57 -0300

    feat: add AddressResponse for ViaCEP API

---
+++ Source/Intermediate/Learning.Asynchronous/Mappings/AddressResponse.cs
@@ -0,0 +1,38 @@
+﻿namespace Learning.Asynchronous.Mappings;
+
+/// <summary>
+/// Representa um endereço 
+/// retornado pela API do ViaCEP.
+/// </summary>
+internal sealed record AddressResponse
+{
+    /// <summary>
+    /// Unidade Federativa.
+    /// </summary>
+    [JsonPropertyName("uf")]
+    public string? State { get; init; }
+
+    /// <summary>
+    /// Nome da cidade.
+    /// </summary>
+    [JsonPropertyName("localidade")]
+    public string? City { get; init; }
+
+    /// <summary>
+    /// Nome da rua.
+    /// </summary>
+    [JsonPropertyName("logradouro")]
+    public string? Street { get; init; }
+
+    /// <summary>
+    /// Código de Endereçamento Postal.
+    /// </summary>
+    [JsonPropertyName("cep")]
+    public string? ZipCode { get; init; }
+
+    /// <summary>
+    /// Indica se houve erro na busca.
+    /// </summary>
+    [JsonPropertyName("erro")]
+    public string? Error { get; init; }
+}
