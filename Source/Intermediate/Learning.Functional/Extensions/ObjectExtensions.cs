0776d69f0009d475c26630deb0a6d1299697e12d
Brendo Pereira Viegas <159083802+Viegasync@users.noreply.github.com>
2025-08-28 04:19:58 -0300

    feat: add AsEnumerable extension method

---
+++ Source/Intermediate/Learning.Functional/Extensions/ObjectExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Learning.Functional.Extensions;
+
+/// <summary>
+/// Método extensivo
+/// </summary>
+internal static class ObjectExtensions
+{
+    /// <summary>
+    /// Converte um objeto em 
+    /// uma sequência enumerável.
+    /// </summary>
+    internal static IEnumerable<T> AsEnumerable<T>(this T? item)
+    {
+        if (item is null) yield break;
+        yield return item;
+    }
+}
