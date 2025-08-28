f7b0b29580417b8143cf03508062e02f5a414ca4
Brendo Pereira Viegas <159083802+Viegasync@users.noreply.github.com>
2025-08-28 04:23:10 -0300

    chore: add System.Text and Extensions namespaces

--- Source/Intermediate/Learning.Functional/Application/GlobalUsings.cs
+++ Source/Intermediate/Learning.Functional/Application/GlobalUsings.cs
@@ -1,4 +1,6 @@
 ﻿global using System.Reflection;
+global using System.Text;
 global using Learning.Functional.Application;
+global using Learning.Functional.Extensions;
 global using Learning.Functional.Models;
 global using Learning.Functional.Services;
