86f9e2ebd1ee2519224a207b97040a5dd39794ad
Brendo Pereira Viegas <159083802+Viegasync@users.noreply.github.com>
2025-08-28 04:48:40 -0300

    feat: add DisplayErrors and DisplayProperties methods

---
+++ Source/Intermediate/Learning.Asynchronous/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,66 @@
+﻿namespace Learning.Asynchronous.Extensions;
+
+/// <summary>
+/// Método extensivo.
+/// </summary>
+internal static class EnumerableExtensions
+{
+    /// <summary>
+    /// Exibe no console todas 
+    /// as mensagens de erro da coleção.
+    /// </summary>
+    internal static void DisplayErrors<TSource>(this IEnumerable<TSource> source)
+    {
+        DisplayIfEmpty(source, "No errors.");
+        foreach (TSource? item in source)
+            Console.WriteLine(item);
+    }
+
+    /// <summary>
+    /// Exibe no console todas as propriedades
+    /// não nulas de cada item da coleção.
+    /// </summary>
+    internal static void DisplayProperties<TSource>(this IEnumerable<TSource> source)
+    {
+        DisplayIfEmpty(source);
+        foreach (TSource item in source)
+            Console.WriteLine(GetPropertyValue(item));
+    }
+
+    /// <summary>
+    /// Retorna o valor de todas
+    /// as propriedades não nulas.
+    /// </summary>
+    private static StringBuilder GetPropertyValue<T>(T? item)
+    {
+        StringBuilder builder = new();
+
+        PropertyInfo[] properties = [.. typeof(T)
+            .GetProperties()
+            .Where(property =>
+                property.GetIndexParameters().Length == 0)
+            ];
+
+        foreach (PropertyInfo property in properties)
+        {
+            if (item is null) continue;
+
+            object? currentValue = property.GetValue(item);
+
+            if (currentValue is string value && !string.IsNullOrWhiteSpace(value))
+                builder.AppendLine($"{property.Name}: {value}");
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Imprime uma mensagem no console 
+    /// caso a coleção esteja vazia.
+    /// </summary>
+    private static void DisplayIfEmpty<TSource>(IEnumerable<TSource> source, string message = "Empty")
+    {
+        if (!source.Any())
+            Console.WriteLine(message);
+    }
+}
