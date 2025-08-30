c69a97a9671ac9780f094188e7c692c2936af730
Brendo Pereira Viegas <159083802+Viegasync@users.noreply.github.com>
2025-08-29 21:35:16 -0300

    refactor: make all properties non-nullable

--- Source/Intermediate/Learning.Asynchronous/Models/AddressResponse.cs
+++ Source/Intermediate/Learning.Asynchronous/Models/AddressResponse.cs
@@ -10,29 +10,29 @@ internal sealed record AddressResponse
     /// Unidade Federativa.
     /// </summary>
     [JsonPropertyName("uf")]
-    public string? State { get; init; }
+    public string State { get; init; }
 
     /// <summary>
     /// Nome da cidade.
     /// </summary>
     [JsonPropertyName("localidade")]
-    public string? City { get; init; }
+    public string City { get; init; }
 
     /// <summary>
     /// Nome da rua.
     /// </summary>
     [JsonPropertyName("logradouro")]
-    public string? Street { get; init; }
+    public string Street { get; init; }
 
     /// <summary>
     /// Código de Endereçamento Postal.
     /// </summary>
     [JsonPropertyName("cep")]
-    public string? ZipCode { get; init; }
+    public string ZipCode { get; init; }
 
     /// <summary>
     /// Indica se houve erro na busca.
     /// </summary>
     [JsonPropertyName("erro")]
-    public string? Error { get; init; }
+    public string Error { get; init; }
 }
